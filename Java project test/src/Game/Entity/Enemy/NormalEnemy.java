ca016673b6927a1c2812dcda20677c6879700b6d
G.N.U.R.T <55143782+lhtrung11@users.noreply.github.com>
2019-11-09 22:22:07 +0700

    xóa
    xóa

--- Java project test/src/Game/Entity/Enemy/NormalEnemy.java
+++
@@ -1,8 +0,0 @@
-package Game.Entity.Enemy;
-import Game.Entity.GameTiles.Tile.TileGrid;
-
-public class NormalEnemy extends Enemy{
-    public NormalEnemy(int x, int y, TileGrid grid) {
-        super(x, y, grid);
-    }
-}
